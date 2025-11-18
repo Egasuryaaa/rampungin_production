fba4dd88dd5c474fdda110e8149ddb25197ed022
ega <egasurya04@gmail.com>
2025-11-18 11:45:27 +0700

    fix

--- src/utils/prisma.util.js
+++ src/utils/prisma.util.js
@@ -1,4 +1,4 @@
 // File: src/utils/prisma.util.js
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require('../generated/client'); // <-- Ganti ke path lokal
 const prisma = new PrismaClient();
 module.exports = prisma;
